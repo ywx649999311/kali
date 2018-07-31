6b8bd1c3116ee1385409b3a9e5132e6b6e6cb380
ywx649999311 <wy73@drexel.edu>
2018-07-31 16:49:02 -0400

    bug: fix pyplot not found

--- python/kali/__init__.py
+++ python/kali/__init__.py
@@ -1,7 +1,9 @@
+"""Init file."""
 import os
 from shutil import copy
 import matplotlib as mpl
 import matplotlib.style
+import matplotlib.pyplot
 from kali.util.mpl_settings import plot_params
 
 # mpl style path
@@ -11,12 +13,12 @@ style_path = os.path.join(confi_dir, 'stylelib/')
 
 # check path exists, if not, create
 if not os.path.exists(style_path):
-	os.makedirs(style_path)
+    os.makedirs(style_path)
 
 # check if style copied, if not, copy and update library
 if not os.path.exists(os.path.join(style_path, 'kali.mplstyle')):
-	copy(os.path.join(kali_dir, 'python/kali/util/kali.mplstyle'), style_path)
-	mpl.pyplot.style.reload_library()
+    copy(os.path.join(kali_dir, 'python/kali/util/kali.mplstyle'), style_path)
+    mpl.pyplot.style.reload_library()
 
 # default to use 'kali' style
 mpl.style.use('kali')
